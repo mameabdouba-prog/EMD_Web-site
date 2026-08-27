34fb87f92468412c51a67bd64360da9a28efe81c
Dabakh Ba <mameabdou.ba@uadb.edu.sn>
2026-08-27 23:27:48 +0000

    feat: auto-provision admin superuser on app ready

--- core/apps.py
+++ core/apps.py
@@ -1,3 +1,4 @@
+import os
 from django.apps import AppConfig
 
 
@@ -6,11 +7,27 @@ class CoreConfig(AppConfig):
     name = 'core'
     verbose_name = 'Gestion EMD'
 
-    from django.apps import AppConfig
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
-
     def ready(self):
         import core.signals
+        self.ensure_superuser()
+
+    def ensure_superuser(self):
+        try:
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+            username = os.environ.get('ADMIN_USERNAME', 'admin')
+            email = os.environ.get('ADMIN_EMAIL', 'admin@gs-emd.com')
+            password = os.environ.get('ADMIN_PASSWORD', 'admin123')
+
+            user = User.objects.filter(username=username).first()
+            if user:
+                user.set_password(password)
+                user.is_superuser = True
+                user.is_staff = True
+                user.is_active = True
+                user.save()
+            else:
+                User.objects.create_superuser(username=username, email=email, password=password)
+        except Exception:
+            # Ignorer si la base n'est pas encore migrée lors d'un command de management initial
+            pass
