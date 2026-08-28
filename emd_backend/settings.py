71b3300acdaaef6279940b25b8b4bb8e9b779a87
Dabakh Ba <mameabdou.ba@uadb.edu.sn>
2026-08-28 20:13:33 +0000

    fix: disable DRF SessionAuthentication to prevent 403 CSRF on stateles API

--- emd_backend/settings.py
+++ emd_backend/settings.py
@@ -137,6 +137,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
 
 # ===================== EMAIL (GMAIL SMTP) =====================
